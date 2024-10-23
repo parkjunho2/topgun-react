c23f18fea8f4b76f9bfd42b54587574a2979683b
daisyyb <happy1339k@gmail.com>
2024-10-23 13:15:37 +0900

    오류재수정

--- topgun-react/src/components/notice/notice.js
+++ topgun-react/src/components/notice/notice.js
@@ -3,10 +3,11 @@ import { FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import ReactQuill from "react-quill";
+import { useRecoilValue } from "recoil"; // Recoil 상태 불러오기
+import { loginState, userState } from "../../util/recoil"; // Recoil atoms
 import 'react-quill/dist/quill.snow.css';
-import './noticeButton.css'; // 프레임 라인 버튼 스타일을 포함한 CSS 파일
+//import './noticeButton.css'; // 프레임 라인 버튼 스타일을 포함한 CSS 파일
 
-//업데이트 테스트
 const NoticeBoard = () => {
     const [noticeList, setNoticeList] = useState([]);
     const [input, setInput] = useState({
@@ -15,8 +16,11 @@ const NoticeBoard = () => {
         author: "",
         createdAt: new Date().toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' }),
     });
+    const quillRef = useRef(null);
 
-    const quillRef = useRef(null); // Quill 인스턴스를 위한 ref 추가
+    // 로그인 상태와 사용자 정보 가져오기
+    const login = useRecoilValue(loginState);
+    const user = useRecoilValue(userState);
 
     useEffect(() => {
         loadList();
@@ -61,10 +65,15 @@ const NoticeBoard = () => {
     }, []);
 
     const addInput = useCallback(async () => {
+        if (!login) {
+            alert("로그인이 필요합니다.");
+            return;
+        }
+
         const newNotice = {
             title: input.title,
             content: input.content,
-            author: input.author,
+            author: `${user.userId} (${user.userType})`, // ID와 타입을 함께 설정
             createdAt: new Date().toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' }),
         };
 
@@ -76,7 +85,7 @@ const NoticeBoard = () => {
             console.error("Failed to add notice:", error);
             alert("공란을 모두 채워 주세요!.");
         }
-    }, [input, loadList]);
+    }, [input, loadList, login, user]);
 
     const clearInput = useCallback(() => {
         setInput({
@@ -87,10 +96,6 @@ const NoticeBoard = () => {
         });
     }, []);
 
-    const threeDaysAgo = new Date();
-    threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
-
-    // 이미지 업로드 핸들러
     const handleImageUpload = () => {
         const input = document.createElement('input');
         input.setAttribute('type', 'file');
@@ -101,15 +106,15 @@ const NoticeBoard = () => {
             const file = input.files[0];
             const reader = new FileReader();
             reader.onloadend = () => {
-                const quill = quillRef.current.getEditor(); // Quill 인스턴스 가져오기
+                const quill = quillRef.current.getEditor();
                 const range = quill.getSelection();
                 if (range) {
-                    quill.insertEmbed(range.index, 'image', reader.result); // 데이터 URL을 사용하여 이미지 삽입
-                    quill.setSelection(range.index + 1, 0); // 커서를 이미지 뒤로 이동
+                    quill.insertEmbed(range.index, 'image', reader.result);
+                    quill.setSelection(range.index + 1, 0);
                 }
             };
             if (file) {
-                reader.readAsDataURL(file); // 파일을 URL로 변환
+                reader.readAsDataURL(file);
             }
         };
     };
@@ -120,22 +125,21 @@ const NoticeBoard = () => {
                 [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
                 [{ 'list': 'ordered' }, { 'list': 'bullet' }],
                 ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-                ['link', 'image'], // 이미지 버튼 추가
+                ['link', 'image'],
                 [{ 'align': [] }],
                 [{ 'color': [] }, { 'background': [] }],
                 ['clean']
             ],
             handlers: {
-                'image': handleImageUpload // 이미지 버튼 핸들러
+                'image': handleImageUpload
             }
         }
-    }), []); // 빈 배열로 의존성을 지정하여 재렌더링 방지
+    }), []);
 
     return (
         <div className="row mt-4">
             <div className="col" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <h1 style={{ textAlign: 'center', marginBottom: '10px' }}>NOTICE</h1>
-
                 <hr style={{ width: '80%', border: '1px solid #e3305f', marginBottom: '18px' }} />
                 <table className="table" style={{ width: '80%', tableLayout: 'fixed' }}>
                     <thead>
@@ -150,85 +154,74 @@ const NoticeBoard = () => {
                     <tbody>
                         {noticeList.map((notice) => (
                             <tr key={notice.noticeId}>
-                                <td style={{ padding: '15px', textAlign: 'center' }}>
-                                    {notice.noticeId}
-                                </td>
+                                <td style={{ padding: '15px', textAlign: 'center' }}>{notice.noticeId}</td>
                                 <td style={{ padding: '15px', textAlign: 'center' }}>
                                     <Link to={`/notice/${notice.noticeId}`} style={{ color: 'inherit', textDecoration: 'none' }}>
                                         {notice.title}
                                     </Link>
-                                    {new Date(notice.createdAt) >= threeDaysAgo &&
-                                        <span style={{
-                                            color: 'white',
-                                            backgroundColor: '#ec7393',
-                                            padding: '3px 3px',
-                                            borderRadius: '5px',
-                                            fontSize: '0.9em',
-                                            marginLeft: '10px',
-                                            marginBottom: '10px'
-                                        }}>
-                                            NEW
-                                        </span>}
                                 </td>
                                 <td style={{ padding: '15px', textAlign: 'center' }}>{notice.author}</td>
                                 <td style={{ padding: '15px', textAlign: 'center' }}>{notice.createdAt}</td>
                                 <td style={{ padding: '15px', textAlign: 'center' }}>
-                                    <FaTrash
-                                        className="text-danger trash-icon" // trash-icon 클래스 추가
-                                        style={{ color: '#ec7393', fill: '#ec7393', fontSize: '1em' }}
-                                        onClick={() => deleteNotice(notice)}
-                                    />
-
+                                    {login && ( // 로그인 상태일 때만 쓰레기통 아이콘 표시
+                                        <FaTrash
+                                            className="text-danger trash-icon"
+                                            style={{ color: '#ec7393', fill: '#ec7393', fontSize: '1em' }}
+                                            onClick={() => deleteNotice(notice)}
+                                        />
+                                    )}
                                 </td>
                             </tr>
                         ))}
                     </tbody>
+
                     <tfoot>
-                        <tr>
-                            <td style={{ textAlign: 'center' }}></td>
-                            <td>
-                                <input type="text" className="form-control"
-                                    placeholder="제목"
-                                    name="title"
-                                    value={input.title}
-                                    onChange={changeInput} />
-                            </td>
-                            <td>
-                                <input type="text" className="form-control"
-                                    placeholder="작성자"
-                                    name="author"
-                                    value={input.author}
-                                    onChange={changeInput} />
-                            </td>
-                            <td style={{ textAlign: 'center', padding: '15px' }}>
-                                <div style={{ marginBottom: '52px' }}> {/* 버튼과 아래 선 간격 조정 */}
-                                    <a className="frame-btn" href="#" onClick={addInput}>
-                                        <span className="frame-btn__outline frame-btn__outline--tall">
-                                            <span className="frame-btn__line frame-btn__line--tall"></span>
-                                            <span className="frame-btn__line frame-btn__line--flat"></span>
-                                        </span>
-                                        <span className="frame-btn__outline frame-btn__outline--flat">
-                                            <span className="frame-btn__line frame-btn__line--tall"></span>
-                                            <span className="frame-btn__line frame-btn__line--flat"></span>
-                                        </span>
-                                        <span className="frame-btn__solid"></span>
-                                        <span className="frame-btn__text">POST NOTICE</span>
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colSpan="4" style={{ padding: '15px' }}>
-                                <ReactQuill
-                                    ref={quillRef} // ref 추가
-                                    value={input.content}
-                                    onChange={handleContentChange}
-                                    modules={modules}
-                                    placeholder="오른쪽 아래 선택자로 에디터 크기를 자유롭게 조절하세요!"
-                                    style={{ height: '290px', width: '110%', resize: 'vertical', overflowY: 'auto' }} // 너비를 100%로 설정
-                                />
-                            </td>
-                        </tr>
+                        {login && ( // 로그인한 사용자만 입력 폼 표시
+                            <>
+                                <tr>
+                                    <td style={{ textAlign: 'center' }}></td>
+                                    <td>
+                                        <input type="text" className="form-control"
+                                            placeholder="제목"
+                                            name="title"
+                                            value={input.title}
+                                            onChange={changeInput} />
+                                    </td>
+                                    <td>
+
+                                    </td>
+                                    <td style={{ textAlign: 'center', padding: '15px' }}>
+                                        <div style={{ marginBottom: '52px' }}>
+                                            <a className="frame-btn" href="#" onClick={addInput}>
+                                                <span className="frame-btn__outline frame-btn__outline--tall">
+                                                    <span className="frame-btn__line frame-btn__line--tall"></span>
+                                                    <span className="frame-btn__line frame-btn__line--flat"></span>
+                                                </span>
+                                                <span className="frame-btn__outline frame-btn__outline--flat">
+                                                    <span className="frame-btn__line frame-btn__line--tall"></span>
+                                                    <span className="frame-btn__line frame-btn__line--flat"></span>
+                                                </span>
+                                                <span className="frame-btn__solid"></span>
+                                                <span className="frame-btn__text">POST NOTICE</span>
+                                            </a>
+                                        </div>
+                                    </td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td colSpan="4" style={{ padding: '15px' }}>
+                                        <ReactQuill
+                                            ref={quillRef} // ref 추가
+                                            value={input.content}
+                                            onChange={handleContentChange}
+                                            modules={modules}
+                                            placeholder="오른쪽 아래 선택자로 에디터 크기를 자유롭게 조절하세요!"
+                                            style={{ height: '290px', width: '110%', resize: 'vertical', overflowY: 'auto' }} // 너비를 100%로 설정
+                                        />
+                                    </td>
+                                </tr>
+                            </>
+                        )}
                     </tfoot>
                 </table>
             </div>
