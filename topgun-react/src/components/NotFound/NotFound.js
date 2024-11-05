fdf9ec9235cd7b197ffa704438d0efe4986c97b6
IhubeI <jkl5914@naver.com>
2024-11-05 16:41:04 +0900

    console.log 주석처리 및 404NotFoundPage수정

--- topgun-react/src/components/NotFound/NotFound.js
+++ topgun-react/src/components/NotFound/NotFound.js
@@ -1,12 +1,15 @@
 const NotFound = () => {
-    return (
-      <div className="container text-center mt-5">
-        <h1 className="display-1">404</h1>
-        <h2 className="display-4">페이지를 찾을 수 없습니다.</h2>
-        <p className="lead">찾고 있는 페이지는 존재하지 않거나, URL이 잘못 입력되었을 수 있습니다.</p>
-        <a href="/" className="btn btn-primary">홈으로 돌아가기</a>
-      </div>
-    );
-  };
-  
-  export default NotFound;
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="display-1">404</h1>
+      <h2 className="display-4">페이지를 찾을 수 없습니다.</h2>
+      <p className="lead">찾고 있는 페이지는 존재하지 않거나, URL이 잘못 입력되었을 수 있습니다.</p>
+      <button type="button" className="btn btn-primary"
+        onClick={e => navigate(-1)}>
+        뒤로가기
+      </button>
+    </div>
+  );
+};
+
+export default NotFound;
